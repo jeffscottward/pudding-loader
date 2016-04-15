4a55329be690ca715164db8049b5ff815d1fee4b
Jeff Scott Ward <jsward.17@gmail.com>
2016-04-14 21:17:41 -0400

    working definitely when loaders are in the right order - pudding comes first

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 var Pudding = require('ether-pudding');
 var PuddingGenerator = require('ether-pudding/generator');
 
-module.exports = function(compiledSolObj) { 
+module.exports = function(compiledSolObj) {
 
 	this.cacheable();
 
 	var classContracts = "";
-	
+
+	if(Pudding.version    === undefined) { Pudding.version    = '0.0.0'; }
+
 	for (var contractName in compiledSolObj.contracts){
 
-		console.log(contractName);
-
 		var inputCompiled = compiledSolObj.contracts[contractName];
-		
-	 	classContracts += PuddingGenerator.generate(contractName, inputCompiled);
+
+	 	classContracts += PuddingGenerator.generate(contractName, inputCompiled, Pudding);
 	}
 
-	return classContracts; 
+	return classContracts;
 }
